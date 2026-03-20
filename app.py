6a24cd5d8fce112cea38a44abb16ac63c4aa6895
Sakibu Mumuni <sakibumumuni285@gmail.com>
2026-03-20 08:31:36 +0000

    Database: Progress made

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 import os
 import pymongo
 import bson.json_util
+from dotenv import load_dotenv
+
 app = Flask(__name__)
+
+load_dotenv()
 
 mongodb_url = os.environ.get('MONGODB_URL')
 client = pymongo.MongoClient(mongodb_url)
@@ -13,21 +17,24 @@ print(client.list_database_names())
 @app.route('/invoice', methods = ['POST', 'GET'])
 def get_invoices ():
     if request.method == 'POST':
-      invoices_data = {
-     'client_name':'client_name',
-     'client_address':'client_address',
-     'contact_person':'contact_person',
-     'delivery_date':'delivery_date',
-     'description':'description',
-     'quantity':'quantity',
-     'unit_price':'unit_price',
-     'total':'total',
-     'sub_total':'sub_total',
-     'total_amount':'total_amount',
-     'amount_words':'amount_words'
-      }
-      db.insert_one(invoices_data)
-      return Response(bson.json_util.dumps())
+       print(request.form)
+       invoices_data = {
+           'client_name':request.form.get('client_name'),
+           'client_address':request.form.get('client_address'),
+           'contact_person':request.form.get('contact_person'),
+           'delivery_date':request.form.get('delivery_date'),
+           'description':request.form.get('description'),
+           'quantity':request.form.get('quantity'),
+           'unit_price':request.form.get('unit_price'),
+           'total':request.form.get('total'),
+           'sub_total':request.form.get('sub_total'),
+           'total_amount':request.form.get('total_amount'),
+           'amount_word':request.form.get('amount_words')
+           }
+       db.invoices.insert_one(invoices_data)
+       
+    
+       #return Response(bson.json_util.dumps(invoices_data), mimetype='application/json')
     return render_template('index.html')
  
   
