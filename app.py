705c0d1a3e8f1be53f11887c9ddbea0fae59220d
Sakibu Mumuni <sakibumumuni285@gmail.com>
2026-03-27 17:35:52 +0000

    updated

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def get_invoices():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
 
 
 
