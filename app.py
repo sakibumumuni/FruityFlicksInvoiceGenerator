1e5ea74f3c96ded7462f220cb49f1e303248d734
sakibu <sakibumumuni285@gmail.com>
2026-05-09 06:33:42 +0000

    remove: removed accidental gitignore commit

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, request
-import pymongo
-import bson.json_util
+from flask import Flask, render_template, request, json
+from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 
@@ -8,28 +7,29 @@ load_dotenv()
 
 app = Flask(__name__)
 
-
-mongodb_uri = os.environ.get("mongodb_uri")
-
-client = pymongo.MongoClient(mongodb_uri)
-db = client.fruityflicks
+client = MongoClient(os.getenv("mongodb_uri"))
+db = client.invoices
 print(client.list_database_names)
 
 
-@app.route('/invoice/v1.0', methods = ['GET'])
+@app.route('/invoice', methods = ['GET', 'POST'])
 def invoice_details ():
-    invoice_contact = {
-    'client_name':request.form.get('client_name'),
-    'client_address':request.form.get('client_address'),
-    'contact_person':request.form.get('contact_person'),
-    'delivery_date':request.form.get('delivery_date'),
-    'description[]':request.form.get('description[]'),
-    'quantity[]':request.form.get('quantity[]'),
+    if request.method == 'POST':
+     invoice_contact = {
+     'client_name':request.form.get('client_name'),
+     'client_address':request.form.get('client_address'),
+     'contact_person':request.form.get('contact_person'),
+     'delivery_date':request.form.get('delivery_date'),
+     'description[]':request.form.get('description[]'),
+     'quantity[]':request.form.get('quantity[]'),
     }
-    db.fruityflicks.insert_one(invoice_contact)
+     db.invoices.insert_one(invoice_contact)
     return render_template('index.html')
 
 
+
+
+#json_string = json.dumps(invoice_contact)
 
 if  __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
